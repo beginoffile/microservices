06c31d8807bf37b4d219ca5702bc76498f5eb24b
Cesar <cursos.mapatupisaro@gmail.com>
2023-11-16 16:07:17 -0600

    finishing the communications between services using RPC

--- logger-service/cmd/api/main.go
+++ logger-service/cmd/api/main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"log"
 	"log-service/data"
+	"net"
 	"net/http"
+	"net/rpc"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -50,6 +52,10 @@ func main() {
 		Models: data.New(client),
 	}
 
+	// Register the RPC server
+	err = rpc.Register(new(RPCServer))
+	go app.rpcListen()
+
 	// start web server
 	// go app.serve()
 	//app.serve()
@@ -78,6 +84,27 @@ func main() {
 // 	}
 
 // }
+
+func (app *Config) rpcListen() error {
+	log.Println("Starting RPC Server on port " + rpcPort)
+
+	listen, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%s", rpcPort))
+
+	if err != nil {
+		return err
+	}
+
+	defer listen.Close()
+
+	for {
+		rpcConn, err := listen.Accept()
+		if err != nil {
+			continue
+		}
+		go rpc.ServeConn(rpcConn)
+	}
+
+}
 
 func connectToMongo() (*mongo.Client, error) {
 	// create conection options
