3ae8b4967ba43884fef6ef8244f34904c29466b3
Cesar <cursos.mapatupisaro@gmail.com>
2023-11-22 09:50:39 -0600

    adding front-end to docker swarm

--- front-end/cmd/web/main.go
+++ front-end/cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"embed"
 	"fmt"
 	"html/template"
 	"log"
@@ -12,29 +13,33 @@ func main() {
 		render(w, "test.page.tmpl")
 	})
 
-	fmt.Println("Starting front end service on port 80")
-	err := http.ListenAndServe(":80", nil)
+	fmt.Println("Starting front end service on port 8081")
+	err := http.ListenAndServe(":8081", nil)
 	if err != nil {
 		log.Panic(err)
 	}
 }
 
+// go:embed templates
+var templateFS embed.FS
+
 func render(w http.ResponseWriter, t string) {
 
 	partials := []string{
-		"./cmd/web/templates/base.layout.tmpl",
-		"./cmd/web/templates/header.partial.tmpl",
-		"./cmd/web/templates/footer.partial.tmpl",
+		"templates/base.layout.tmpl",
+		"templates/header.partial.tmpl",
+		"templates/footer.partial.tmpl",
 	}
 
 	var templateSlice []string
-	templateSlice = append(templateSlice, fmt.Sprintf("./cmd/web/templates/%s", t))
+	templateSlice = append(templateSlice, fmt.Sprintf("templates/%s", t))
 
 	for _, x := range partials {
 		templateSlice = append(templateSlice, x)
 	}
 
-	tmpl, err := template.ParseFiles(templateSlice...)
+	// tmpl, err := template.ParseFiles(templateSlice...)
+	tmpl, err := template.ParseFS(templateFS, templateSlice...)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
